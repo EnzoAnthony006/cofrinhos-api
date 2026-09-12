b4d99ddd9ae18a8daf29330ad082d8a6eb06f7d9
Enzo Anthony <enzoanthonydev06@gmail.com>
2026-09-12 11:49:25 -0300

    the interface for save and search for ID

--- cofrinhos-api/src/main/java/com/enzoanthonydev/cofrinhos/cofrinhos_api/application/CofrinhoRepository.java
+++ cofrinhos-api/src/main/java/com/enzoanthonydev/cofrinhos/cofrinhos_api/application/CofrinhoRepository.java
@@ -1,4 +1,13 @@
 package com.enzoanthonydev.cofrinhos.cofrinhos_api.application;
 
-public class CofrinhoRepository {
+import com.enzoanthonydev.cofrinhos.cofrinhos_api.Domain.Cofrinho;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface  CofrinhoRepository {
+
+    Cofrinho salvar (Cofrinho cofrinho);
+
+    Optional<Cofrinho> buscarPorId (UUID id);
 }
