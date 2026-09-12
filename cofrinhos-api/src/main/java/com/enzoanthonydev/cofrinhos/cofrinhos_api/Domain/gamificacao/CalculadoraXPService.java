8b68ce9777d8e641ede4aa4922886144226c2503
Enzo Anthony <enzoanthonydev06@gmail.com>
2026-09-12 11:28:53 -0300

    the class CalculadoraXPService Dontthe class CalculadoraXPService Dont are an entity, dont have entity, dont have state, just take data and return an number.

--- cofrinhos-api/src/main/java/com/enzoanthonydev/cofrinhos/cofrinhos_api/Domain/gamificacao/CalculadoraXPService.java
+++ cofrinhos-api/src/main/java/com/enzoanthonydev/cofrinhos/cofrinhos_api/Domain/gamificacao/CalculadoraXPService.java
@@ -1,4 +1,27 @@
 package com.enzoanthonydev.cofrinhos.cofrinhos_api.Domain.gamificacao;
 
+import com.enzoanthonydev.cofrinhos.cofrinhos_api.Domain.Dinheiro;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 public class CalculadoraXPService {
+
+    private static final int XP_BASE = 10;
+    private static final BigDecimal DIVISOR_PROPORCIONAL = BigDecimal.TEN;
+    private static final int TETO_PROPORCIONAL = 50;
+    private static final int BONUS_POR_SEMANA_DE_STREAK = 5;
+
+
+    public int calcular ( Dinheiro valorAporte, int semanaDeStreak) {
+       int xpProporcional = calcularXpProporcional (valorAporte);
+       int xpStreak = semanaDeStreak * BONUS_POR_SEMANA_DE_STREAK;
+       return XP_BASE + xpProporcional + xpStreak;
+    }
+
+    private int calcularXpProporcional (Dinheiro valorAporte) {
+        BigDecimal proporcional = valorAporte.valor()
+                .divide(DIVISOR_PROPORCIONAL, 0 , RoundingMode.DOWN);
+        return  Math.min(proporcional.intValue(), TETO_PROPORCIONAL);
+    }
 }
