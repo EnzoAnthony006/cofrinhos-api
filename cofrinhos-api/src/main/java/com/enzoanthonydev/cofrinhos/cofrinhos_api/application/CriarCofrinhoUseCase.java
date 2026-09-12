8a1d3b2b7ea97d3fd0c74c7347c421cc8d71b62b
Enzo Anthony <enzoanthonydev06@gmail.com>
2026-09-12 11:53:46 -0300

    the function for create piggy bank and execute

--- cofrinhos-api/src/main/java/com/enzoanthonydev/cofrinhos/cofrinhos_api/application/CriarCofrinhoUseCase.java
+++ cofrinhos-api/src/main/java/com/enzoanthonydev/cofrinhos/cofrinhos_api/application/CriarCofrinhoUseCase.java
@@ -1,4 +1,24 @@
 package com.enzoanthonydev.cofrinhos.cofrinhos_api.application;
 
+
+import com.enzoanthonydev.cofrinhos.cofrinhos_api.Domain.CategoriaInvestimento;
+import com.enzoanthonydev.cofrinhos.cofrinhos_api.Domain.Cofrinho;
+import com.enzoanthonydev.cofrinhos.cofrinhos_api.Domain.Dinheiro;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
 public class CriarCofrinhoUseCase {
+
+    private final CofrinhoRepository cofrinhoRepository;
+
+    public CriarCofrinhoUseCase(CofrinhoRepository cofrinhoRepository) {
+        this.cofrinhoRepository = cofrinhoRepository;
+    }
+    public Cofrinho executar (UUID usuarioId, String nome, String descricao,
+                              CategoriaInvestimento categoria, Dinheiro valorMeta) {
+        Cofrinho cofrinho = Cofrinho.criar(usuarioId, nome, descricao, categoria, valorMeta);
+        return cofrinhoRepository.salvar(cofrinho);
+    }
 }
