c123a8af4e5800253ed15837a4ce408d8e8f0adf
Enzo Anthony <enzoanthonydev06@gmail.com>
2026-09-12 11:20:04 -0300

    the class ProfileXP, where register, iniciate  and add, all about profileXP

--- cofrinhos-api/src/main/java/com/enzoanthonydev/cofrinhos/cofrinhos_api/Domain/gamificacao/PerfilXP.java
+++ cofrinhos-api/src/main/java/com/enzoanthonydev/cofrinhos/cofrinhos_api/Domain/gamificacao/PerfilXP.java
@@ -1,4 +1,41 @@
 package com.enzoanthonydev.cofrinhos.cofrinhos_api.Domain.gamificacao;
 
+import java.time.LocalDate;
+import java.util.UUID;
+
 public class PerfilXP {
+
+    private final UUID usuarioId;
+    private int xpTotal;
+    private final Streak streakGlobal;
+
+    private PerfilXP(UUID usuarioId, int xpTotal, Streak streakGlobal) {
+        this.usuarioId = usuarioId;
+        this.xpTotal = xpTotal;
+        this.streakGlobal = streakGlobal;
+    }
+    public static PerfilXP iniciar (UUID usuarioId) {
+        return new PerfilXP(usuarioId,0, Streak.iniciar());
+    }
+    public void adicionarXP (int quantidade) {
+        if (quantidade < 0) {
+            throw new IllegalArgumentException("Quantidade de XP não pode ser negativa");
+        }
+        this.xpTotal += quantidade;
+    }
+    public void registrarAtividadeStreak(LocalDate data) {
+        streakGlobal.registrarAtividade(data);
+    }
+    public int getNivel() {
+        return (xpTotal / 500 ) + 1;
+    }
+    public UUID getUsuarioId() {
+        return usuarioId;
+    }
+    public int getXpTotal() {
+        return xpTotal;
+    }
+    public Streak getStreakGlobal() {
+        return streakGlobal;
+    }
 }
