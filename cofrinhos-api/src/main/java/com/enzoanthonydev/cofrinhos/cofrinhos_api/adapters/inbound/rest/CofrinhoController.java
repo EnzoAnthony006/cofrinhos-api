f8db77ecfcc6eef02bdcd182eac025df9ef883f3
Enzo Anthony <enzoanthonydev06@gmail.com>
2026-09-16 17:07:00 -0300

    Get Mapping ( id ) in Cofrinho Controller and private final BuscarCofrinhoUseCase

--- cofrinhos-api/src/main/java/com/enzoanthonydev/cofrinhos/cofrinhos_api/adapters/inbound/rest/CofrinhoController.java
+++ cofrinhos-api/src/main/java/com/enzoanthonydev/cofrinhos/cofrinhos_api/adapters/inbound/rest/CofrinhoController.java
@@ -2,20 +2,25 @@ package com.enzoanthonydev.cofrinhos.cofrinhos_api.adapters.inbound.rest;
 
 import com.enzoanthonydev.cofrinhos.cofrinhos_api.Domain.Cofrinho;
 import com.enzoanthonydev.cofrinhos.cofrinhos_api.Domain.Dinheiro;
+import com.enzoanthonydev.cofrinhos.cofrinhos_api.application.BuscarCofrinhoUseCase;
 import com.enzoanthonydev.cofrinhos.cofrinhos_api.application.CriarCofrinhoUseCase;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.UUID;
+
 @RestController
 @RequestMapping("/cofrinhos")
 public class CofrinhoController {
 
     private final CriarCofrinhoUseCase criarCofrinhoUseCase;
+    private final BuscarCofrinhoUseCase buscarCofrinhoUseCase;
 
-    public CofrinhoController(CriarCofrinhoUseCase criarCofrinhoUseCase) {
+    public CofrinhoController(CriarCofrinhoUseCase criarCofrinhoUseCase, BuscarCofrinhoUseCase buscarCofrinhoUseCase) {
         this.criarCofrinhoUseCase = criarCofrinhoUseCase;
+        this.buscarCofrinhoUseCase = buscarCofrinhoUseCase;
     }
 
     @PostMapping
@@ -28,5 +33,11 @@ public class CofrinhoController {
                 Dinheiro.de(request.valorMeta())
         );
         return ResponseEntity.status(HttpStatus.CREATED).body(CofrinhoResponse.de(cofrinho));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<CofrinhoResponse> buscarPorId(@PathVariable UUID id) {
+        Cofrinho cofrinho = buscarCofrinhoUseCase.executar(id);
+        return ResponseEntity.ok(CofrinhoResponse.de(cofrinho));
     }
 }
