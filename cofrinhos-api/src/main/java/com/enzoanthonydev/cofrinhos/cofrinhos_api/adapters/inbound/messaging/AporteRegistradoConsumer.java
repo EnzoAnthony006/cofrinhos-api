47e0061477a2a040abf3385c3189541ef1a3a56a
Enzo Anthony <enzoanthonydev06@gmail.com>
2026-09-23 17:40:37 -0300

    changes with AporteRegistradoConsumer, new public for process consumer

--- cofrinhos-api/src/main/java/com/enzoanthonydev/cofrinhos/cofrinhos_api/adapters/inbound/messaging/AporteRegistradoConsumer.java
+++ cofrinhos-api/src/main/java/com/enzoanthonydev/cofrinhos/cofrinhos_api/adapters/inbound/messaging/AporteRegistradoConsumer.java
@@ -1,11 +1,18 @@
 package com.enzoanthonydev.cofrinhos.cofrinhos_api.adapters.inbound.messaging;
 
+import com.enzoanthonydev.cofrinhos.cofrinhos_api.Domain.gamificacao.ProcessarAporteRegistradoUseCase;
 import com.enzoanthonydev.cofrinhos.cofrinhos_api.application.aporte.AporteRegistradoEvent;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AporteRegistradoConsumer {
+
+    private final ProcessarAporteRegistradoUseCase processarAporteRegistradoUseCase;
+
+    public AporteRegistradoConsumer(ProcessarAporteRegistradoUseCase processarAporteRegistradoUseCase) {
+        this.processarAporteRegistradoUseCase = processarAporteRegistradoUseCase;
+    }
 
     @KafkaListener(topics = "aporte-registrado" , groupId = "cofrinhos-api" )
     public void consumir (AporteRegistradoEvent evento){
